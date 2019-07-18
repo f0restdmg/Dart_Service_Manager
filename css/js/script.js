a43cc9851a4e7a0a2d4b2bd60a772b7afff6d467
f0restdmg <40428470+f0restdmg@users.noreply.github.com>
2019-07-18 10:28:59 +0400

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,11 +1,15 @@
-
-    var acc = document.getElementsByClassName("service-acc__header");
-    var text = document.getElementsByClassName(".service-acc__body.show");
-
+var acc = document.getElementsByClassName("service-acc__header");
   for (var i = 0; i < acc.length; i++){
       acc[i].onclick = function(){
         this.nextElementSibling.classList.toggle("service-acc__body");
         }
           
       }
-  
+
+ $(document).ready(function(){
+  $('.customers__slider').slick({
+    vertical : true,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  });
+}); 
