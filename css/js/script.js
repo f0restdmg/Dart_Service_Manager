faf9fcdcec88dd3ab2414eded270575bb020e320
f0restdmg <40428470+f0restdmg@users.noreply.github.com>
2019-07-23 01:57:57 +0400

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -10,6 +10,21 @@ var acc = document.getElementsByClassName("service-acc__header");
   $('.customers__slider').slick({
     vertical : true,
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    nextArrow: '<i class="fa fa-angle-down" aria-hidden="true"></i>',
+    prevArrow: '<i class="fa fa-angle-up" aria-hidden="true"></i>',
   });
 }); 
+
+const person = document.querySelectorAll('.person-photo');
+  person.forEach((elem) => {
+    elem.addEventListener('click', (e) => {
+      person.forEach((item) => {
+        if (item === e.target) {
+          item.classList.add('active')
+          return
+        }
+        item.classList.remove('active')
+      })
+    })
+  })
